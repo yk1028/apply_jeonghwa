bd31d75e9055ed7cd63e76f64ca8cae6d37c341e
mohok <pnujhseo@pusan.ac.kr>
2018-12-05 18:05:00 +0900

    Initial Commit

---
+++ boxOfficeForBoostCamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  boxOfficeForBoostCamp
+//
+//  Created by 정화 on 05/12/2018.
+//  Copyright © 2018 mohomogu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
