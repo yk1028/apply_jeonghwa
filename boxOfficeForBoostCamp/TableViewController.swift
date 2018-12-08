7750e07ebae936d4d4a0b10c7be45a9445b41eab
mohok <pnujhseo@pusan.ac.kr>
2018-12-08 12:47:23 +0900

    테이블 탭 페이지 테이블셀 레이아웃 설정

--- boxOfficeForBoostCamp/TableViewController.swift
+++ boxOfficeForBoostCamp/TableViewController.swift
@@ -29,7 +29,10 @@ class TableTabViewController: UITableViewController {
         return tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
     }
     
-    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 100
+    }
 
 }
 
@@ -40,7 +43,18 @@ class tableTabCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupViews()
         backgroundColor = .cyan
+        self.imageView?.image = UIImage(named: "movieSample")
+
     }
+    
+    
+    let movieImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
     
     let movieTitle: UILabel = {
         let label = UILabel()
@@ -50,11 +64,50 @@ class tableTabCell: UITableViewCell {
         return label
     }()
     
+    let movieSubTitle: UILabel = {
+        let label = UILabel()
+        label.text = "평점 : 10 예매순위 : 10 예매율 : 10.10"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let movieReleaseDate: UILabel = {
+        let label = UILabel()
+        label.text = "개봉일 : 2000-11-11"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
+    
     func setupViews() {
-        addSubview(movieTitle)
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": movieTitle]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[v0]-10-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": movieTitle]))
+        
+        let insetFromImage: CGFloat = 10
+        
+        contentView.addSubview(movieImage)
+        movieImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
+        movieImage.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+        movieImage.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        movieImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
 
+        contentView.addSubview(movieTitle)
+        movieTitle.leftAnchor.constraint(equalTo: movieImage.rightAnchor, constant: insetFromImage).isActive = true
+        movieTitle.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+//        movieTitle.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
+        movieTitle.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+
+        contentView.addSubview(movieSubTitle)
+        movieSubTitle.leftAnchor.constraint(equalTo: movieImage.rightAnchor, constant: insetFromImage).isActive = true
+        movieSubTitle.topAnchor.constraint(equalTo: movieTitle.bottomAnchor, constant: 10).isActive = true
+//        movieSubTitle.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        movieSubTitle.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
+        contentView.addSubview(movieReleaseDate)
+        movieReleaseDate.leftAnchor.constraint(equalTo: movieImage.rightAnchor, constant: insetFromImage).isActive = true
+        movieReleaseDate.topAnchor.constraint(equalTo: movieSubTitle.bottomAnchor, constant: 10).isActive = true
+//        movieReleaseDate.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        movieReleaseDate.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         
     }
     
